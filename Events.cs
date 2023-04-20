2c963ec2b271a8b59d0605366b49bc92fe937265
Sovanno <124546977+Sovanno@users.noreply.github.com>
2023-04-20 10:08:58 +0600

    Update Events.cs

--- Events.cs
+++ Events.cs
@@ -8,38 +8,97 @@ namespace ConsoleApp2
 {
     internal class Class1
     {
-        delegate void Delegate (int n);
+        delegate void Delegate();
+        public static double x;
+        public static double y;
         class MyEvent
         {
-            public event Delegate SomeEvent;
-            public void OnSomeEvent(int n)
+            Delegate[] evnt = new Delegate[4];
+            public event Delegate SomeEvent
             {
-                if (SomeEvent != null)
-                    SomeEvent(n);
+                add
+                {
+                    int i;
+                    for (i = 0; i < 4; i++)
+                        if (evnt[i] == null)
+                        {
+                            evnt[i] = value;
+                            break;
+                        }
+                    if (i == 3) Console.WriteLine("Список событий заполнен.");
+                }
+                remove
+                {
+                    int i;
+                    for (i = 0; i < 4; i++)
+                        if (evnt[i] == value)
+                        {
+                            evnt[i] = null;
+                            break;
+                        }
+                    if (i == 3) Console.WriteLine("Обработчик событий не найден.");
+                }
+            }
+            public void OnSomeEvent()
+            {
+                for (int i = 0; i < 4; i++)
+                    if (evnt[i] != null) evnt[i]();
+            }
+
+        }
+        class Summ
+        {
+            public void Summer()
+            {
+                Console.WriteLine(x + y);
+            }
+        }
+        class Minus
+        {
+            public void Minuser()
+            {
+                Console.WriteLine(x - y);
+            }
+        }
+        class Multi
+        {
+            public void Multier()
+            {
+                Console.WriteLine(x * y);
+            }
+        }
+        class Division
+        {
+            public void Divisioner()
+            {
+                if (y != 0)
+                {
+                    Console.WriteLine(x / y);
+                }
+                else
+                {
+                    Console.WriteLine("На ноль не делим");
+                }
             }
         }
 
         static void Main()
         {
-            double x, y;
             MyEvent myEvent = new MyEvent();
+            Summ summ = new Summ();
+            Minus minus = new Minus();
+            Multi multi = new Multi();
+            Division division = new Division();
 
             x = Convert.ToDouble(Console.ReadLine());
             y = Convert.ToDouble(Console.ReadLine());
 
-            myEvent.SomeEvent += (n) => Console.WriteLine(x + y);
-            myEvent.SomeEvent += (n) => Console.WriteLine(x - y);
-            myEvent.SomeEvent += (n) => Console.WriteLine(x * y);
-            if (y != 0) 
-            {
-                myEvent.SomeEvent += (n) => Console.WriteLine(x / y);
-            }
-            else
-            {
-                myEvent.SomeEvent += (n) => Console.WriteLine("На ноль не делим");
-            }
+            myEvent.SomeEvent += summ.Summer;
+            myEvent.SomeEvent += minus.Minuser;
+            myEvent.SomeEvent += multi.Multier;
+            myEvent.SomeEvent += division.Divisioner;
 
-            myEvent.OnSomeEvent(1);
+            myEvent.OnSomeEvent();
         }
     }
 }
